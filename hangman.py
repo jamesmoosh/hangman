dfe5e59918a4ba2a49af8735822176ad6a93edbb
James Gay <jamesrkg@gmail.com>
2016-10-10 14:34:27 +0100

    progam planned out

--- hangman.py
+++ hangman.py
@@ -1,2 +1,26 @@
 ## hangman game
 ## 10/10/16 -jrkg
+
+## input word from dictionary
+
+## reject words <3, >8 letters long
+
+## print _ _ _ (length of word)
+
+## ask for guess
+
+## check guess is a letter, and hasn't already been guessed. add to list of guesses
+
+## check if guess is in word
+
+## if yes, then new working word is  _ _ _ x _ replacing instances of letter. if word is complete then win
+
+## if no add line to hangman. if hangman finished then lose
+
+## print hangman
+
+## print list of guesses
+
+## print working word
+
+## ask for guess
