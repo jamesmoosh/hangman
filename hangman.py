f6ba70d4093c559f2d08897ca9a4a2eabd8e5502
James Gay <jamesrkg@gmail.com>
2016-10-10 15:17:07 +0100

    first few stages done

--- hangman.py
+++ hangman.py
@@ -1,11 +1,35 @@
 ## hangman game
 ## 10/10/16 -jrkg
 
-## input word from dictionary
+import random
+
+dictionary = open(dictionary.txt)
+dict_length = 0
+while True:
+    buffer = dictionary.readline()
+    if buffer == "":
+        break
+    dict_length += 1
+    
+line_number = random.randint(1,dict_length)
+
+## input random word from dictionary
+
+i = 0
+while i < line_number:
+    word_to_guess = dictionary.readline()
+    i += 1
+
+word_to_guess = word_to_guess[:-2]
 
 ## reject words <3, >8 letters long
 
+while len(word_to_guess)< 3 or len(word_to_guess) > 8:
+    word_to_guess = 
+
 ## print _ _ _ (length of word)
+working_word = str("_ " * len(word_to_guess))
+print("Word to guess is" working_word)
 
 ## ask for guess
 
